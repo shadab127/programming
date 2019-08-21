fdecbeb221124535cc8f5a187dd8f4a9c257123c
shadab khan <shadabkhan127@gmail.com>
2019-08-21 14:20:49 +0530

    p2 completed

--- codeforces/581div2/p2.cpp
+++ codeforces/581div2/p2.cpp
@@ -2,5 +2,37 @@
  * @Author: shadabKhan
  * @Date:   Wednesday, August 21st 2019, 2:00:20 pm
  * @Last modified by:   shadabKhan
- * @Last modified time: Wednesday, August 21st 2019, 2:00:20 pm
+ * @Last modified time: Wednesday, August 21st 2019, 2:19:29 pm
  */
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n,l,r;
+  cin>>n>>l>>r;
+  vector<int64_t>arr1(n,1);
+  vector<int64_t>arr2(n,1);
+  int64_t temp=1;
+  for(int i=0;i<r;i++)
+  {
+    if(i<l)
+    {
+      arr1[i]=temp;
+      arr2[i]=temp;
+    }
+    else if(i<r)
+    {
+      arr2[i]=temp;
+    }
+    temp=temp<<1;
+  }
+  temp=temp>>1;
+  for(int i=r;i<n;i++)
+  {
+    arr2[i] = temp;
+  }
+  int64_t mini=0,maxi=0;
+  for(int i=0;i<n;i++)mini+=arr1[i];
+  for(int i=0;i<n;i++)maxi+=arr2[i];
+  cout<<mini<<" "<<maxi<<endl;
+}
